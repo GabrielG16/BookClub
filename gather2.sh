6c12c9936270014a148fd13ff032dafb149f72f0
Gabriel Guedes <gabriellguedes16@gmail.com>
2022-04-21 18:36:14 -0300

    Bash Scripts

---
+++ gather2.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+/home/gabriel/anaconda3/envs/co2/bin/python /home/gabriel/Desktop/Projetos/BookClub/scraper.py >> /home/gabriel/Desktop/Projetos/BookClub/log.txt
---
+++ gather_books.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+
+conda activate co2
+
+python /home/gabriel/Desktop/Projetos/BookClub/scraper.py
